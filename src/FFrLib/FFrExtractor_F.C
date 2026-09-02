4f06df721d8faa17bf8f4f9190e6a449bfb6888d
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2026-09-02 07:57:40 +0200

    Move doxy from FFrExtractor_F.C to FFrExtractorInterface.f90

--- src/FFrLib/FFrExtractor_F.C
+++ src/FFrLib/FFrExtractor_F.C
@@ -11,7 +11,18 @@
 
   \details This file contains some global-scope functions callable from Fortran.
   It only wraps the functionality needed by the solvers for accessing the RDB,
-  e.g., during recovery and/or restart operations.
+  e.g., during recovery and/or restart operations. This includes the following:
+
+  - ffrextractorinterface::ffr_init
+  - ffrextractorinterface::ffr_done
+  - ffrextractorinterface::ffr_findptr
+  - ffrextractorinterface::ffr_setposition
+  - ffrextractorinterface::ffr_increment
+  - ffrextractorinterface::ffr_getdata
+  - ffrextractorinterface::ffr_finddata
+
+  No further documentation is provided here.
+  The above wrappers are documented in the FFrExtractorInterface.f90 file.
 
   \author Knut Morten Okstad
   \date 9 Oct 2000
@@ -25,8 +36,41 @@
 #include "FFaLib/FFaOS/FFaFilePath.H"
 #include "FFaLib/FFaOS/FFaFortran.H"
 
-static FFrExtractor* ourRdb  = NULL; //!< Pointer to the extractor object
-static FFrEntryBase* stepPtr = NULL; //!< Pointer to the step number variable
+
+namespace
+{
+  FFrExtractor* ourRdb  = NULL; //!< Pointer to the extractor object
+  FFrEntryBase* stepPtr = NULL; //!< Pointer to the step number variable
+
+  //! \brief Helper for finding file position of a result variable.
+  FFrEntryBase* findPtr(const std::string& path,
+                        const std::string& ogType, int baseId,
+#if FFR_DEBUG > 1
+                        const char* dbgMsg
+#else
+                        const char*
+#endif
+ ){
+    FFaResultDescription entry;
+    entry.baseId = baseId;
+    entry.OGType = ogType;
+
+    // Split up the path description into a vector of strings
+    size_t iStart = 0, iEnd, nChr;
+    while (iStart != std::string::npos)
+    {
+      iEnd = path.find('|',iStart);
+      nChr = iEnd == std::string::npos ? iEnd : iEnd-iStart;
+      entry.varDescrPath.push_back(path.substr(iStart,nChr));
+      iStart = iEnd == std::string::npos ? iEnd : iEnd+1;
+    }
+
+#if FFR_DEBUG > 1
+    std::cout << dbgMsg << entry;
+#endif
+    return ourRdb->search(entry);
+  }
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -88,58 +132,7 @@ SUBROUTINE(ffr_done,FFR_DONE) ()
 
 
 ////////////////////////////////////////////////////////////////////////////////
-//! \brief Static helper for finding file position of a result variable.
-//! \param[in] path Variable description
-//! \param[in] ogType Type name of the object we are searching results for
-//! \param[in] baseId Unique identifier of the object we are searching for
-//!
-//! \details This function computes the relative file position pointer within a
-//! time step for the data pertaining to the specified variable or item group,
-//! within the specified owner group identified by \a ogType and \a baseId.
-//!
-//! The variable- or item group specification string \a path contains the
-//! full path of the wanted object in the result database hierarchy using
-//! the '|' character to separate between the different levels.
-//!
-//! \author Knut Morten Okstad
-//! \date 14 Nov 2000
-////////////////////////////////////////////////////////////////////////////////
-
-static FFrEntryBase* findPtr(const std::string& path, const std::string& ogType,
-#if FFR_DEBUG > 1
-                             int baseId, const char* dbgMsg = NULL
-#else
-                             int baseId, const char* = NULL
-#endif
-){
-  FFaResultDescription entry;
-  entry.baseId = baseId;
-  entry.OGType = ogType;
-
-  // Split up the path description into a vector of strings
-  size_t iStart = 0, iEnd, nChr;
-  while (iStart != std::string::npos)
-  {
-    iEnd = path.find('|',iStart);
-    nChr = iEnd == std::string::npos ? iEnd : iEnd-iStart;
-    entry.varDescrPath.push_back(path.substr(iStart,nChr));
-    iStart = iEnd == std::string::npos ? iEnd : iEnd+1;
-  }
-
-#if FFR_DEBUG > 1
-  if (dbgMsg) std::cout << dbgMsg << entry;
-#endif
-  return ourRdb->search(entry);
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
 //! \brief Finds file position of the specified result variable.
-//! \sa findPtr()
-//!
-//! \author Knut Morten Okstad
-//! \date 14 Nov 2000
-////////////////////////////////////////////////////////////////////////////////
 
 SUBROUTINE(ffr_findptr,FFR_FINDPTR) (const char* pathName,
 #ifdef _NCHAR_AFTER_CHARARG
@@ -162,16 +155,6 @@ SUBROUTINE(ffr_findptr,FFR_FINDPTR) (const char* pathName,
 
 ////////////////////////////////////////////////////////////////////////////////
 //! \brief Locates results data within current time step for specified variable.
-//! \details Finds the data pertaining to the specified variable or item group
-//! within the owner group identified by \a objectType and \a baseId.
-//!
-//! The variable- or item group specification string \a pathName contains the
-//! full path of the wanted object in the result database hierarchy using
-//! the '|' character to separate between the different levels.
-//!
-//! \author Knut Morten Okstad
-//! \date 24 Jun 2002
-////////////////////////////////////////////////////////////////////////////////
 
 SUBROUTINE(ffr_realdata,FFR_REALDATA) (const double* data, const f90_int& nw,
 #ifdef _NCHAR_AFTER_CHARARG
@@ -200,16 +183,6 @@ SUBROUTINE(ffr_realdata,FFR_REALDATA) (const double* data, const f90_int& nw,
 
 ////////////////////////////////////////////////////////////////////////////////
 //! \brief Locates results data within current time step for specified variable.
-//! \details Finds the data pertaining to the specified integer variable
-//! within the owner group identified by \a objectType and \a baseId.
-//!
-//! The variable specification string \a pathName contains the
-//! full path of the wanted object in the result database hierarchy using
-//! the '|' character to separate between the different levels.
-//!
-//! \author Knut Morten Okstad
-//! \date 24 Jun 2002
-////////////////////////////////////////////////////////////////////////////////
 
 SUBROUTINE(ffr_intdata,FFR_INTDATA) (const f90_int* data, const f90_int& nw,
 #ifdef _NCHAR_AFTER_CHARARG
@@ -239,18 +212,6 @@ SUBROUTINE(ffr_intdata,FFR_INTDATA) (const f90_int* data, const f90_int& nw,
 
 ////////////////////////////////////////////////////////////////////////////////
 //! \brief Positions the results file(s) for the specified time step.
-//! \details Position all result containers of the extractor such that the next
-//! ffr_getdata() operations read result data from the time step corresponding
-//! to the physical time \a atime (or the step that is closest to this time).
-//! The physical time of the time step that is found is returned through
-//! the variable \a btime.
-//!
-//! \note Current implementation finds the next larger position through use of
-//! the boolean flag \a getNextHigh.
-//!
-//! \author Knut Morten Okstad
-//! \date 14 Nov 2000
-////////////////////////////////////////////////////////////////////////////////
 
 SUBROUTINE(ffr_setposition,FFR_SETPOSITION) (const double& atime,
                                              double& btime, f90_int8& istep)
@@ -276,14 +237,6 @@ SUBROUTINE(ffr_setposition,FFR_SETPOSITION) (const double& atime,
 
 ////////////////////////////////////////////////////////////////////////////////
 //! \brief Positions the results file(s) for the next time step.
-//! \details Increment sll result containers of the extractor such that the next
-//! ffr_getdata() operations read result data from the next time step, if any.
-//! The physical time of the time step that is found is returned through
-//! the variable \a btime.
-//!
-//! \author Knut Morten Okstad
-//! \date 21 May 2001
-////////////////////////////////////////////////////////////////////////////////
 
 SUBROUTINE(ffr_increment,FFR_INCREMENT) (double& btime, f90_int8& istep)
 {
@@ -311,12 +264,6 @@ SUBROUTINE(ffr_increment,FFR_INCREMENT) (double& btime, f90_int8& istep)
 
 ////////////////////////////////////////////////////////////////////////////////
 //! \brief Reads the contents of the variable or item group.
-//! \details Reads the data pointed to by \a varPtr within current time step,
-//! as set by ffr_setposition() from the results database.
-//!
-//! \author Knut Morten Okstad
-//! \date 14 Nov 2000
-////////////////////////////////////////////////////////////////////////////////
 
 SUBROUTINE(ffr_getdata,FFR_GETDATA) (const double* data, const f90_int& nw,
                                      const ptr_int& varPtr, f90_int& ierr)
